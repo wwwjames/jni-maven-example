681390219122e425af32302fb8b7ca1bec58e2f9
komesergey <komesergey@gmail.com>
2020-05-29 23:51:58 +0300

    Added example how to call object methods, static methods and create objects from JNI code

--- src/main/java/com/jni/example/JniWrapper.java
+++ src/main/java/com/jni/example/JniWrapper.java
@@ -1,9 +1,25 @@
 package com.jni.example;
 
+import java.util.List;
+
+@SuppressWarnings("unused")
 public class JniWrapper {
     private static final String LIB_NAME = "jnilibrary";
     static {
         System.loadLibrary(LIB_NAME);
     }
     public native String getString();
+
+    public void callback(String string) {
+        System.out.println("Callback: " + string);
+    }
+
+    public void callbackObject(List<String> arrayList) {
+        System.out.println("Callback object: " + arrayList.size());
+    }
+
+    public static void callbackStatic(String string) {
+        System.out.println("Static callback: " + string);
+    }
+
 }
--- src/native/source/jni.c
+++ src/native/source/jni.c
@@ -1,10 +1,37 @@
 #include "com_jni_example_JniWrapper.h"
 #include <stdio.h>
 
-#define DEFAULT_RET "HELLO WORLD !!!"
+#define DEFAULT_RET "Hello world!"
+#define DEFAULT_CALLBACK "Hello world callback!"
+#define DEFAULT_CALLBACK_STATIC "Hello world static callback!"
 
-JNIEXPORT jstring JNICALL Java_com_jni_example_JniWrapper_getString
-  (JNIEnv *env, jobject thiz) {
+JNIEXPORT jstring JNICALL Java_com_jni_example_JniWrapper_getString(JNIEnv *env, jobject thiz) {
+    jclass cls_foo = (*env)->GetObjectClass(env, thiz);
+    jmethodID mid_callback = (*env)->GetMethodID(env, cls_foo, "callback", "(Ljava/lang/String;)V");
+    jmethodID mid_callback_object = (*env)->GetMethodID(env, cls_foo, "callbackObject", "(Ljava/util/List;)V");
+    jmethodID mid_callback_static = (*env)->GetStaticMethodID(env, cls_foo, "callbackStatic", "(Ljava/lang/String;)V");
+
+    char* string_regular = DEFAULT_CALLBACK;
+    jstring jStringRegular = (*env)->NewStringUTF(env, string_regular);
+
+    char* string_static = DEFAULT_CALLBACK_STATIC;
+    jstring jStringStatic = (*env)->NewStringUTF(env, string_static);
+
+    (*env)->CallVoidMethod(env, thiz, mid_callback, jStringRegular);
+    (*env)->CallStaticVoidMethod(env, thiz, mid_callback_static, jStringStatic);
+
+    (*env)->DeleteLocalRef(env, jStringRegular);
+    (*env)->DeleteLocalRef(env, jStringStatic);
+
+    jclass cls = (*env)->FindClass(env, "java/util/ArrayList");
+    jmethodID constructor = (*env)->GetMethodID(env, cls, "<init>", "()V");
+
+    jobject arraylist = (*env)->NewObject(env, cls, constructor);
+
+    (*env)->CallVoidMethod(env, thiz, mid_callback_object, arraylist);
+
+    (*env)->DeleteLocalRef(env, arraylist);
+
     char* ret = DEFAULT_RET;
     return (*env)->NewStringUTF(env, ret);
 }
